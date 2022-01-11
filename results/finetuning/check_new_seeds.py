71e2daf46b6b35222a92cea8292f34bec870eab6
samGMIT <g00292346@gmit.ie>
2022-01-11 12:39:38 -0300

    fixed check_new_seeds.py under results/finetuning

--- results/finetuning/check_new_seeds.py
+++ results/finetuning/check_new_seeds.py
@@ -5,6 +5,7 @@ cwd = os.path.abspath(scriptname+"/..")
 
 filename = cwd+"/results08-12-21.txt"
 filename = cwd+"/results10-12-21.txt"
+filename = cwd+"/results08-01-22.txt"
 
 with open(filename, "r+", encoding="utf-8") as f:
     lines = f.readlines()
@@ -12,19 +13,25 @@ with open(filename, "r+", encoding="utf-8") as f:
 ob_lines = [l for l in lines if "only_bert" in l]
 b_lines = [l for l in lines if l not in ob_lines]
 
+print("b_lines:", len(b_lines))
+print("ob_lines:", len(ob_lines))
 
-TrainShape:(1182, 837) #EPOCH: ([], 200, 'bert', 0.0001, 128, 0.3, 32) AVG: 0.7452020913559375 VAR: 0.0005738122241386669 F1: 0.743987339765227 SEED: 30507
+#TrainShape:(1182, 837) #EPOCH: ([], 200, 'bert', 0.0001, 128, 0.3, 32) AVG: 0.7452020913559375 VAR: 0.0005738122241386669 F1: 0.743987339765227 SEED: 30507
 
 f1s, accs, accs2, diffs, seeds =  [],[],[],[],[]
 for i,e in enumerate(zip(b_lines,ob_lines)):
     b,ob = e
     s_b = ''.join(b.split(" ")[6:9])[:-1]
     s_ob = ''.join(ob.split(" ")[6:9])[:-1]
+    seed_b = b.split(" ")[-1]
+    seed_ob = ob.split(" ")[-1]
 
     if s_b != s_ob:
-        print(i)
-        print(s_b, s_ob)
+        print(i*2)
+        print(s_b+" "+seed_b)
+        print(s_ob+" "+seed_ob)
         exit(1)
+
     acc = float("{:.4f}".format(float(b.split(" ")[11])))
     accs.append(acc)
 
@@ -46,5 +53,9 @@ pairs2 = pairs.copy()
 sorted_h_acc = sorted(pairs, key=lambda x: x[0], reverse=True)
 sorted_h_dif = sorted(pairs2, key=lambda x: x[2], reverse=True)
 
+print("sorted by acc_B")
+print("acc_b", "acc_ob", "diff", "seed", "f1")
 print(sorted_h_acc[:5])
+print("sorted by DIFF")
+print("acc_b", "acc_ob", "diff", "seed", "f1")
 print(sorted_h_dif[:5])
